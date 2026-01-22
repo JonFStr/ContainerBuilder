2ba70db73b046bd9cc001b5c399d6e714179092a
Jonathan Straub <j.f.straub@gmx.de>
2026-01-22 10:11:54 +0100

    Add git author configuration envvars

--- builder.sh
+++ builder.sh
@@ -8,13 +8,19 @@ then
 	docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS" "$REGISTRY_HOST"
 fi
 
+if [ -z "$GIT_AUTHOR_NAME" -o -z "$GIT_AUTHOR_EMAIL" ]
+then
+	export GIT_AUTHOR_NAME="Automated Container Builder"
+	export GIT_AUTHOR_EMAIL="builder@registry.local"
+fi
+
 rebuild_image() {
 	# Obtain image name
 	name=$(basename "$1")
 
 	if [ -d "$1/.git" ]; then
 		echo Found git repositry, performing pull…
-		git -C "$1" pull --rebase
+		git -c user.name="$GIT_AUTHOR_NAME" -c user.email="$GIT_AUTHOR_EMAIL" -C "$1" pull --rebase
 	fi
 
 	args_file="$(realpath "$1")/../${name}.args.sh"
